1eac4ea586e68a8dfe104e25bbb1a7f700560c7f
vigeee97 <vigeee97@gmail.com>
2024-09-17 00:56:15 +0400

    Update ft_print_comb2.c

--- c_00/ex06/ft_print_comb2.c
+++ c_00/ex06/ft_print_comb2.c
@@ -1,45 +1,36 @@
 #include <unistd.h>
 
-void ft_print_comb2(void)
+void	ft_print_comb2(void)
 {
-    int a;
-    int b;
-    char c;
-    char d = ' ';
-    char e = ',';
+	int		a;
+	int		b;
+	char	c[5];
 
-    for (a = 0; a <= 98; a++)
-    {
-        for (b = a + 1; b <= 99; b++)
-        {
-            // Печатаем первую часть комбинации (a)
-            c = '0' + (a / 10);
-            write(1, &c, 1);
-            c = '0' + (a % 10);
-            write(1, &c, 1);
-
-            // Печатаем пробел между числами
-            write(1, &d, 1);
-
-            // Печатаем вторую часть комбинации (b)
-            c = '0' + (b / 10);
-            write(1, &c, 1);
-            c = '0' + (b % 10);
-            write(1, &c, 1);
-
-            // Печатаем запятую и пробел, если это не последняя комбинация
-            if (a != 98 || b != 99)
-            {
-                write(1, &e, 1);
-                write(1, &d, 1);
-            }
-        }
-    }
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			c[0] = '0' + (a / 10);
+			c[1] = '0' + (a % 10);
+			c[2] = ' ';
+			c[3] = '0' + (b / 10);
+			c[4] = '0' + (b % 10);
+			write(1, c, 5);
+			if (a != 98 || b != 99)
+			{
+				write(1, ", ", 2);
+			}
+			b++;
+		}
+		a++;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    ft_print_comb2();
-    return 0;
+	ft_print_comb2 ();
+	return (0);
 }
 
